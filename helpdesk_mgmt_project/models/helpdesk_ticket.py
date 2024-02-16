21af396c5615982a80f0a245a3a1fc8ea2b7ea81
aronabencherif <aronabencherif.diatta@xcg.africa>
2024-02-16 10:41:24 +0000

    [ADD] helpdesk_mgmt_project: Set by default the company as the project one in tickets

--- helpdesk_mgmt_project/models/helpdesk_ticket.py
+++ helpdesk_mgmt_project/models/helpdesk_ticket.py
@@ -6,12 +6,22 @@ class HelpdeskTicket(models.Model):
     _inherit = "helpdesk.ticket"
 
     project_id = fields.Many2one(string="Project", comodel_name="project.project")
+
     task_id = fields.Many2one(
         string="Task",
         comodel_name="project.task",
         compute="_compute_task_id",
         readonly=False,
         store=True,
+    )
+
+    # Override field
+    company_id = fields.Many2one(
+        default=lambda self: self.env["project.project"]
+        .browse(self._context["default_project_id"])
+        .company_id
+        if self._context.get("default_project_id")
+        else self.env.company
     )
 
     @api.depends("project_id")
